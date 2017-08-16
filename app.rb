89ff116658cf0be730322b4c4709fd8eabeeea0d
Jenny Bacon <jenniferbacon01@gmail.com>
2017-08-16 16:35:45 +0100

    post redirect get pattern

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @names = params[:name]
+    @name_1 = params[:name_1]
+    @name_2 = params[:name_2]
+    erb(:play)
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect "/play"
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
     erb(:play)
   end
 
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -2,13 +2,14 @@ feature "testing forms" do
 
   it "has a form on the homepage" do
     visit '/'
-    expect(page). to have_field("name")
+    expect(page). to have_field("name_1")
   end
 
   it "displays the names entered in the form in /names" do
        visit '/'
-    fill_in "name", :with => "Jenny"
+    fill_in "name_1", :with => "Jenny"
+    fill_in "name_2", :with => "Freddie"
     click_button "Submit"
-    expect(page).to have_content("Jenny")
+    expect(page).to have_content("Jenny vs Freddie")
   end
 end
