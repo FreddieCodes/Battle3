cd0d1a1e24cf7ee3d4776f659851696b4d57c499
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-16 15:46:31 +0100

    Rspec with  Cabybara- Entering Players

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @names = params[:name]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -1,7 +1,14 @@
+feature "testing forms" do
 
-feature "testing infrastructure" do
-  it "homepage says Testing infrastructure working!" do
+  it "has a form on the homepage" do
     visit '/'
-    expect(page).to have_content('Testing infrastructure working!')
+    expect(page). to have_field("name")
+  end
+
+  it "displays the names entered in the form in /names" do
+       visit '/'
+    fill_in "name", :with => "Jenny"
+    click_button "Submit"
+    expect(page).to have_content("Jenny")
   end
 end
